0303bd8f5e49b03dde10b542e7ddeb783e45102d
vlekev <vlekev@outlook.fr>
2023-06-25 21:53:07 +0200

    error component

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
+import { ErrorComponent } from './pages/error/error.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent},
-  { path: '**', redirectTo: '' }
+  { path: 'error', component: ErrorComponent},
+  { path: '**', redirectTo: 'error' }
 ];
 
 @NgModule({
