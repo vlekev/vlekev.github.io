df53713e23a61349696e3625975f562430353c32
vlekev <vlekev@outlook.fr>
2023-06-25 22:01:54 +0200

    spash screen component

---
+++ src/app/enum/splash-animation-type.ts
@@ -0,0 +1,7 @@
+export enum SplashAnimationType {
+  SlideLeft = "slide-left",
+  SlideRight = "slide-right",
+  SlideBottom = "slide-bottom",
+  SlideTop = "slide-top",
+  FadeOut = "fade-out"
+}
---
+++ src/app/splash-screen/splash-screen.component.ts
@@ -0,0 +1,52 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { SplashAnimationType } from "./../enum/splash-animation-type";
+
+@Component({
+  selector: 'app-splash-screen',
+  templateUrl: './splash-screen.component.html',
+  styleUrls: ['./splash-screen.component.scss']
+})
+export class SplashScreenComponent implements OnInit {
+  windowWidth!: string;
+  windowHeight!: string;
+  splashTransition!: string;
+  opacityChange: number = 1;
+  showSplash = true;
+
+  @Input() animationDuration: number = 0.5;
+  @Input() duration: number = 3;
+  @Input() animationType: SplashAnimationType = SplashAnimationType.SlideBottom;
+
+  ngOnInit(): void {
+    setTimeout(() => {
+      let transitionStyle = "";
+      switch (this.animationType) {
+        case SplashAnimationType.SlideLeft:
+          this.windowWidth = "-" + window.innerWidth + "px";
+          transitionStyle = "left " + this.animationDuration + "s";
+          break;
+        case SplashAnimationType.SlideRight:
+          this.windowWidth = window.innerWidth + "px";
+          transitionStyle = "left " + this.animationDuration + "s";
+          break;
+        case SplashAnimationType.SlideBottom:
+          this.windowHeight = window.innerHeight + "px";
+          transitionStyle = "top " + this.animationDuration + "s";
+          break;
+        case SplashAnimationType.SlideTop:
+          this.windowHeight = "-" + window.innerHeight + "px";
+          transitionStyle = "top " + this.animationDuration + "s";
+          break;
+        case SplashAnimationType.FadeOut:
+          transitionStyle = "opacity " + this.animationDuration + "s";
+          this.opacityChange = 0;
+      }
+
+      this.splashTransition = transitionStyle;
+
+      setTimeout(() => {
+        this.showSplash = !this.showSplash;
+      }, this.animationDuration * 1000);
+    }, this.duration * 1000);
+  }
+}
