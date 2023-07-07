5e32306c0d2912233cc9b3832e0fc0cbbe508327
vlekev <vlekev@outlook.fr>
2023-07-07 08:12:22 +0200

    add 404

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-
+  constructor(private router: Router) {
+    let webpath = localStorage.getItem('web-path');
+    if (webpath) {
+      localStorage.removeItem('web-path');
+      this.router.navigate([webpath]);
+    }
+  }
 }
