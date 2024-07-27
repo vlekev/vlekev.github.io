0a78b6a2a35f245aa5f6e399b762b5bdf361f36a
vlekev <vlekev@yahoo.com>
2024-07-27 12:28:29 +0200

    remove spec + add button to install

--- src/app/pages/install/install.component.ts
+++ src/app/pages/install/install.component.ts
@@ -1,9 +1,12 @@
 import { Component } from '@angular/core';
+import {MatButtonModule} from '@angular/material/button';
 
 @Component({
   selector: 'app-install',
   templateUrl: './install.component.html',
   styleUrls: ['./install.component.scss'],
+  standalone: true,
+  imports: [MatButtonModule],
 })
 export class InstallComponent {
 
